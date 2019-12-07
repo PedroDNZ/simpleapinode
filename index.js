ab2aafd9a237d9c7b3ea410db77b72b1f7fc19d4
Pedro Diniz <dnz.pedro@gmail.com>
2019-12-07 11:30:41 -0200

    dotenv

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+require('dotenv').config();
 
 const server = express();
 
@@ -8,6 +9,7 @@ server.use((req,res,next)=>{
     res.header('Access-Control-Allow-Origin','*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
+    console.log(process.env.teste);
     next(); 
 });
 
@@ -74,4 +76,4 @@ server.delete("/cards/:id",checkCard,(req,res)=>{
     res.json(cards);
 });
 
-server.listen(3333);
+server.listen(process.env.port);
